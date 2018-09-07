f6eb4431f0234950fb76544da02d43d82233db85
YAMAHAH <75268967@qq.com>
2018-09-07 18:03:24 +0800

    init

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AboutPage } from './about.page';
-
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxDataGridModule } from 'devextreme-angular';
 @NgModule({
   imports: [
     IonicModule,
+    DxButtonModule,
+    DxDataGridModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: AboutPage }])
